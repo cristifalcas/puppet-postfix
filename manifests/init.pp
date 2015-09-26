4f82ccce535351a5f882253942fe03da7214f7a3
Cristian Falcas <falcas@optymyze.com>
2015-09-26 13:24:16 +0300

    a class definition was accidently added to init

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,7 +10,8 @@ class postfix (
   $debug                 = false,
   $use_tls               = false,
   $remove_other_mta      = false,
-  $set_alternatives      = true,) {
+  $set_alternatives      = true,
+) {
   contain postfix::install
   contain postfix::config
   contain postfix::service
@@ -18,19 +19,4 @@ class postfix (
   Class['postfix::install'] ->
   Class['postfix::config'] ~>
   Class['postfix::service']
-
-	  class { 'postfix':
-	    ensure                => 'latest',
-	    smtp_endpoint_address => "smtp.${::domain}",
-	    smtp_endpoint_port    => '25',
-	    smtp_endpoint_user    => undef,
-	    smtp_endpoint_pass    => undef,
-	    from_domain           => $::domain,
-	    from_user             => 'donotreply',
-	    forward_all_email_to  => undef,
-	    debug                 => false,
-	    use_tls               => false,
-	    remove_other_mta      => false,
-	    set_alternatives      => true,
-	  }
 }
